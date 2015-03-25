b53e84e9e1a3c04bf6d952d487137978f52df250
shalk <xshalk@163.com>
2015-03-25 18:56:57 +0800

    update version

---
+++ completions/cv.bash
@@ -0,0 +1,16 @@
+_cv() {
+  COMPREPLY=()
+  local word="${COMP_WORDS[COMP_CWORD]}"
+
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    COMPREPLY=( $(compgen -W "$(cv commands)" -- "$word") )
+  else
+    local words=("${COMP_WORDS[@]}")
+    unset words[0]
+    unset words[$COMP_CWORD]
+    local completions=$(cv completions "${words[@]}")
+    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+  fi
+}
+
+complete -F _cv cv
